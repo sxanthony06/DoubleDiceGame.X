5d2d980eb48327beda405a9a0ab675015221a59e
Stephen Anthony <sxanthony06@gmail.com>
2018-10-25 06:29:37 -0400

    Initial Commit - Added MCC generated code for configuring oscillator and I/O pins configuration. Specific I/O pins of MCU were chosen in such a way that routing on PCB gets simplified e.g. less vias, jumpers due to less traces criss-crossing each other. Max. sink/source current of PIC18F45K22 is 25mA. If such current is held to 20mA for safety, 2 ledsegments of an AlphaNumeric LED Display can still be driven (10mA each) through one output pin. Paired led segment pins are reflected in names given to MCU output pins such as Display_2BA

---
+++ mcc_generated_files/pin_manager.h
@@ -0,0 +1,305 @@
+/**
+  @Generated Pin Manager Header File
+
+  @Company:
+    Microchip Technology Inc.
+
+  @File Name:
+    pin_manager.h
+
+  @Summary:
+    This is the Pin Manager file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+
+  @Description
+    This header file provides APIs for driver for .
+    Generation Information :
+        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.65.2
+        Device            :  PIC18F45K22
+        Driver Version    :  2.11
+    The generated drivers are tested against the following:
+        Compiler          :  XC8 1.45
+        MPLAB 	          :  MPLAB X 4.15	
+*/
+
+/*
+    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
+    
+    Subject to your compliance with these terms, you may use Microchip software and any 
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
+    license terms applicable to your use of third party software (including open source software) that 
+    may accompany Microchip software.
+    
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+    FOR A PARTICULAR PURPOSE.
+    
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+    SOFTWARE.
+*/
+
+#ifndef PIN_MANAGER_H
+#define PIN_MANAGER_H
+
+#define INPUT   1
+#define OUTPUT  0
+
+#define HIGH    1
+#define LOW     0
+
+#define ANALOG      1
+#define DIGITAL     0
+
+#define PULL_UP_ENABLED      1
+#define PULL_UP_DISABLED     0
+
+// get/set Display_1UP aliases
+#define Display_1UP_TRIS                 TRISBbits.TRISB0
+#define Display_1UP_LAT                  LATBbits.LATB0
+#define Display_1UP_PORT                 PORTBbits.RB0
+#define Display_1UP_WPU                  WPUBbits.WPUB0
+#define Display_1UP_ANS                  ANSELBbits.ANSB0
+#define Display_1UP_SetHigh()            do { LATBbits.LATB0 = 1; } while(0)
+#define Display_1UP_SetLow()             do { LATBbits.LATB0 = 0; } while(0)
+#define Display_1UP_Toggle()             do { LATBbits.LATB0 = ~LATBbits.LATB0; } while(0)
+#define Display_1UP_GetValue()           PORTBbits.RB0
+#define Display_1UP_SetDigitalInput()    do { TRISBbits.TRISB0 = 1; } while(0)
+#define Display_1UP_SetDigitalOutput()   do { TRISBbits.TRISB0 = 0; } while(0)
+#define Display_1UP_SetPullup()          do { WPUBbits.WPUB0 = 1; } while(0)
+#define Display_1UP_ResetPullup()        do { WPUBbits.WPUB0 = 0; } while(0)
+#define Display_1UP_SetAnalogMode()      do { ANSELBbits.ANSB0 = 1; } while(0)
+#define Display_1UP_SetDigitalMode()     do { ANSELBbits.ANSB0 = 0; } while(0)
+
+// get/set Display_1D aliases
+#define Display_1D_TRIS                 TRISBbits.TRISB1
+#define Display_1D_LAT                  LATBbits.LATB1
+#define Display_1D_PORT                 PORTBbits.RB1
+#define Display_1D_WPU                  WPUBbits.WPUB1
+#define Display_1D_ANS                  ANSELBbits.ANSB1
+#define Display_1D_SetHigh()            do { LATBbits.LATB1 = 1; } while(0)
+#define Display_1D_SetLow()             do { LATBbits.LATB1 = 0; } while(0)
+#define Display_1D_Toggle()             do { LATBbits.LATB1 = ~LATBbits.LATB1; } while(0)
+#define Display_1D_GetValue()           PORTBbits.RB1
+#define Display_1D_SetDigitalInput()    do { TRISBbits.TRISB1 = 1; } while(0)
+#define Display_1D_SetDigitalOutput()   do { TRISBbits.TRISB1 = 0; } while(0)
+#define Display_1D_SetPullup()          do { WPUBbits.WPUB1 = 1; } while(0)
+#define Display_1D_ResetPullup()        do { WPUBbits.WPUB1 = 0; } while(0)
+#define Display_1D_SetAnalogMode()      do { ANSELBbits.ANSB1 = 1; } while(0)
+#define Display_1D_SetDigitalMode()     do { ANSELBbits.ANSB1 = 0; } while(0)
+
+// get/set Display_1FE aliases
+#define Display_1FE_TRIS                 TRISBbits.TRISB2
+#define Display_1FE_LAT                  LATBbits.LATB2
+#define Display_1FE_PORT                 PORTBbits.RB2
+#define Display_1FE_WPU                  WPUBbits.WPUB2
+#define Display_1FE_ANS                  ANSELBbits.ANSB2
+#define Display_1FE_SetHigh()            do { LATBbits.LATB2 = 1; } while(0)
+#define Display_1FE_SetLow()             do { LATBbits.LATB2 = 0; } while(0)
+#define Display_1FE_Toggle()             do { LATBbits.LATB2 = ~LATBbits.LATB2; } while(0)
+#define Display_1FE_GetValue()           PORTBbits.RB2
+#define Display_1FE_SetDigitalInput()    do { TRISBbits.TRISB2 = 1; } while(0)
+#define Display_1FE_SetDigitalOutput()   do { TRISBbits.TRISB2 = 0; } while(0)
+#define Display_1FE_SetPullup()          do { WPUBbits.WPUB2 = 1; } while(0)
+#define Display_1FE_ResetPullup()        do { WPUBbits.WPUB2 = 0; } while(0)
+#define Display_1FE_SetAnalogMode()      do { ANSELBbits.ANSB2 = 1; } while(0)
+#define Display_1FE_SetDigitalMode()     do { ANSELBbits.ANSB2 = 0; } while(0)
+
+// get/set Display_1G aliases
+#define Display_1G_TRIS                 TRISBbits.TRISB3
+#define Display_1G_LAT                  LATBbits.LATB3
+#define Display_1G_PORT                 PORTBbits.RB3
+#define Display_1G_WPU                  WPUBbits.WPUB3
+#define Display_1G_ANS                  ANSELBbits.ANSB3
+#define Display_1G_SetHigh()            do { LATBbits.LATB3 = 1; } while(0)
+#define Display_1G_SetLow()             do { LATBbits.LATB3 = 0; } while(0)
+#define Display_1G_Toggle()             do { LATBbits.LATB3 = ~LATBbits.LATB3; } while(0)
+#define Display_1G_GetValue()           PORTBbits.RB3
+#define Display_1G_SetDigitalInput()    do { TRISBbits.TRISB3 = 1; } while(0)
+#define Display_1G_SetDigitalOutput()   do { TRISBbits.TRISB3 = 0; } while(0)
+#define Display_1G_SetPullup()          do { WPUBbits.WPUB3 = 1; } while(0)
+#define Display_1G_ResetPullup()        do { WPUBbits.WPUB3 = 0; } while(0)
+#define Display_1G_SetAnalogMode()      do { ANSELBbits.ANSB3 = 1; } while(0)
+#define Display_1G_SetDigitalMode()     do { ANSELBbits.ANSB3 = 0; } while(0)
+
+// get/set Display_1H aliases
+#define Display_1H_TRIS                 TRISBbits.TRISB4
+#define Display_1H_LAT                  LATBbits.LATB4
+#define Display_1H_PORT                 PORTBbits.RB4
+#define Display_1H_WPU                  WPUBbits.WPUB4
+#define Display_1H_ANS                  ANSELBbits.ANSB4
+#define Display_1H_SetHigh()            do { LATBbits.LATB4 = 1; } while(0)
+#define Display_1H_SetLow()             do { LATBbits.LATB4 = 0; } while(0)
+#define Display_1H_Toggle()             do { LATBbits.LATB4 = ~LATBbits.LATB4; } while(0)
+#define Display_1H_GetValue()           PORTBbits.RB4
+#define Display_1H_SetDigitalInput()    do { TRISBbits.TRISB4 = 1; } while(0)
+#define Display_1H_SetDigitalOutput()   do { TRISBbits.TRISB4 = 0; } while(0)
+#define Display_1H_SetPullup()          do { WPUBbits.WPUB4 = 1; } while(0)
+#define Display_1H_ResetPullup()        do { WPUBbits.WPUB4 = 0; } while(0)
+#define Display_1H_SetAnalogMode()      do { ANSELBbits.ANSB4 = 1; } while(0)
+#define Display_1H_SetDigitalMode()     do { ANSELBbits.ANSB4 = 0; } while(0)
+
+// get/set Display_1BA aliases
+#define Display_1BA_TRIS                 TRISBbits.TRISB5
+#define Display_1BA_LAT                  LATBbits.LATB5
+#define Display_1BA_PORT                 PORTBbits.RB5
+#define Display_1BA_WPU                  WPUBbits.WPUB5
+#define Display_1BA_ANS                  ANSELBbits.ANSB5
+#define Display_1BA_SetHigh()            do { LATBbits.LATB5 = 1; } while(0)
+#define Display_1BA_SetLow()             do { LATBbits.LATB5 = 0; } while(0)
+#define Display_1BA_Toggle()             do { LATBbits.LATB5 = ~LATBbits.LATB5; } while(0)
+#define Display_1BA_GetValue()           PORTBbits.RB5
+#define Display_1BA_SetDigitalInput()    do { TRISBbits.TRISB5 = 1; } while(0)
+#define Display_1BA_SetDigitalOutput()   do { TRISBbits.TRISB5 = 0; } while(0)
+#define Display_1BA_SetPullup()          do { WPUBbits.WPUB5 = 1; } while(0)
+#define Display_1BA_ResetPullup()        do { WPUBbits.WPUB5 = 0; } while(0)
+#define Display_1BA_SetAnalogMode()      do { ANSELBbits.ANSB5 = 1; } while(0)
+#define Display_1BA_SetDigitalMode()     do { ANSELBbits.ANSB5 = 0; } while(0)
+
+// get/set Display_2C aliases
+#define Display_2C_TRIS                 TRISCbits.TRISC4
+#define Display_2C_LAT                  LATCbits.LATC4
+#define Display_2C_PORT                 PORTCbits.RC4
+#define Display_2C_ANS                  ANSELCbits.ANSC4
+#define Display_2C_SetHigh()            do { LATCbits.LATC4 = 1; } while(0)
+#define Display_2C_SetLow()             do { LATCbits.LATC4 = 0; } while(0)
+#define Display_2C_Toggle()             do { LATCbits.LATC4 = ~LATCbits.LATC4; } while(0)
+#define Display_2C_GetValue()           PORTCbits.RC4
+#define Display_2C_SetDigitalInput()    do { TRISCbits.TRISC4 = 1; } while(0)
+#define Display_2C_SetDigitalOutput()   do { TRISCbits.TRISC4 = 0; } while(0)
+#define Display_2C_SetAnalogMode()      do { ANSELCbits.ANSC4 = 1; } while(0)
+#define Display_2C_SetDigitalMode()     do { ANSELCbits.ANSC4 = 0; } while(0)
+
+// get/set Display_2UP aliases
+#define Display_2UP_TRIS                 TRISCbits.TRISC5
+#define Display_2UP_LAT                  LATCbits.LATC5
+#define Display_2UP_PORT                 PORTCbits.RC5
+#define Display_2UP_ANS                  ANSELCbits.ANSC5
+#define Display_2UP_SetHigh()            do { LATCbits.LATC5 = 1; } while(0)
+#define Display_2UP_SetLow()             do { LATCbits.LATC5 = 0; } while(0)
+#define Display_2UP_Toggle()             do { LATCbits.LATC5 = ~LATCbits.LATC5; } while(0)
+#define Display_2UP_GetValue()           PORTCbits.RC5
+#define Display_2UP_SetDigitalInput()    do { TRISCbits.TRISC5 = 1; } while(0)
+#define Display_2UP_SetDigitalOutput()   do { TRISCbits.TRISC5 = 0; } while(0)
+#define Display_2UP_SetAnalogMode()      do { ANSELCbits.ANSC5 = 1; } while(0)
+#define Display_2UP_SetDigitalMode()     do { ANSELCbits.ANSC5 = 0; } while(0)
+
+// get/set Display_2D aliases
+#define Display_2D_TRIS                 TRISCbits.TRISC6
+#define Display_2D_LAT                  LATCbits.LATC6
+#define Display_2D_PORT                 PORTCbits.RC6
+#define Display_2D_ANS                  ANSELCbits.ANSC6
+#define Display_2D_SetHigh()            do { LATCbits.LATC6 = 1; } while(0)
+#define Display_2D_SetLow()             do { LATCbits.LATC6 = 0; } while(0)
+#define Display_2D_Toggle()             do { LATCbits.LATC6 = ~LATCbits.LATC6; } while(0)
+#define Display_2D_GetValue()           PORTCbits.RC6
+#define Display_2D_SetDigitalInput()    do { TRISCbits.TRISC6 = 1; } while(0)
+#define Display_2D_SetDigitalOutput()   do { TRISCbits.TRISC6 = 0; } while(0)
+#define Display_2D_SetAnalogMode()      do { ANSELCbits.ANSC6 = 1; } while(0)
+#define Display_2D_SetDigitalMode()     do { ANSELCbits.ANSC6 = 0; } while(0)
+
+// get/set Display_2FE aliases
+#define Display_2FE_TRIS                 TRISCbits.TRISC7
+#define Display_2FE_LAT                  LATCbits.LATC7
+#define Display_2FE_PORT                 PORTCbits.RC7
+#define Display_2FE_ANS                  ANSELCbits.ANSC7
+#define Display_2FE_SetHigh()            do { LATCbits.LATC7 = 1; } while(0)
+#define Display_2FE_SetLow()             do { LATCbits.LATC7 = 0; } while(0)
+#define Display_2FE_Toggle()             do { LATCbits.LATC7 = ~LATCbits.LATC7; } while(0)
+#define Display_2FE_GetValue()           PORTCbits.RC7
+#define Display_2FE_SetDigitalInput()    do { TRISCbits.TRISC7 = 1; } while(0)
+#define Display_2FE_SetDigitalOutput()   do { TRISCbits.TRISC7 = 0; } while(0)
+#define Display_2FE_SetAnalogMode()      do { ANSELCbits.ANSC7 = 1; } while(0)
+#define Display_2FE_SetDigitalMode()     do { ANSELCbits.ANSC7 = 0; } while(0)
+
+// get/set Display_2G aliases
+#define Display_2G_TRIS                 TRISDbits.TRISD4
+#define Display_2G_LAT                  LATDbits.LATD4
+#define Display_2G_PORT                 PORTDbits.RD4
+#define Display_2G_ANS                  ANSELDbits.ANSD4
+#define Display_2G_SetHigh()            do { LATDbits.LATD4 = 1; } while(0)
+#define Display_2G_SetLow()             do { LATDbits.LATD4 = 0; } while(0)
+#define Display_2G_Toggle()             do { LATDbits.LATD4 = ~LATDbits.LATD4; } while(0)
+#define Display_2G_GetValue()           PORTDbits.RD4
+#define Display_2G_SetDigitalInput()    do { TRISDbits.TRISD4 = 1; } while(0)
+#define Display_2G_SetDigitalOutput()   do { TRISDbits.TRISD4 = 0; } while(0)
+#define Display_2G_SetAnalogMode()      do { ANSELDbits.ANSD4 = 1; } while(0)
+#define Display_2G_SetDigitalMode()     do { ANSELDbits.ANSD4 = 0; } while(0)
+
+// get/set Display_2H aliases
+#define Display_2H_TRIS                 TRISDbits.TRISD5
+#define Display_2H_LAT                  LATDbits.LATD5
+#define Display_2H_PORT                 PORTDbits.RD5
+#define Display_2H_ANS                  ANSELDbits.ANSD5
+#define Display_2H_SetHigh()            do { LATDbits.LATD5 = 1; } while(0)
+#define Display_2H_SetLow()             do { LATDbits.LATD5 = 0; } while(0)
+#define Display_2H_Toggle()             do { LATDbits.LATD5 = ~LATDbits.LATD5; } while(0)
+#define Display_2H_GetValue()           PORTDbits.RD5
+#define Display_2H_SetDigitalInput()    do { TRISDbits.TRISD5 = 1; } while(0)
+#define Display_2H_SetDigitalOutput()   do { TRISDbits.TRISD5 = 0; } while(0)
+#define Display_2H_SetAnalogMode()      do { ANSELDbits.ANSD5 = 1; } while(0)
+#define Display_2H_SetDigitalMode()     do { ANSELDbits.ANSD5 = 0; } while(0)
+
+// get/set Display_2BA aliases
+#define Display_2BA_TRIS                 TRISDbits.TRISD6
+#define Display_2BA_LAT                  LATDbits.LATD6
+#define Display_2BA_PORT                 PORTDbits.RD6
+#define Display_2BA_ANS                  ANSELDbits.ANSD6
+#define Display_2BA_SetHigh()            do { LATDbits.LATD6 = 1; } while(0)
+#define Display_2BA_SetLow()             do { LATDbits.LATD6 = 0; } while(0)
+#define Display_2BA_Toggle()             do { LATDbits.LATD6 = ~LATDbits.LATD6; } while(0)
+#define Display_2BA_GetValue()           PORTDbits.RD6
+#define Display_2BA_SetDigitalInput()    do { TRISDbits.TRISD6 = 1; } while(0)
+#define Display_2BA_SetDigitalOutput()   do { TRISDbits.TRISD6 = 0; } while(0)
+#define Display_2BA_SetAnalogMode()      do { ANSELDbits.ANSD6 = 1; } while(0)
+#define Display_2BA_SetDigitalMode()     do { ANSELDbits.ANSD6 = 0; } while(0)
+
+// get/set Display_1C aliases
+#define Display_1C_TRIS                 TRISDbits.TRISD7
+#define Display_1C_LAT                  LATDbits.LATD7
+#define Display_1C_PORT                 PORTDbits.RD7
+#define Display_1C_ANS                  ANSELDbits.ANSD7
+#define Display_1C_SetHigh()            do { LATDbits.LATD7 = 1; } while(0)
+#define Display_1C_SetLow()             do { LATDbits.LATD7 = 0; } while(0)
+#define Display_1C_Toggle()             do { LATDbits.LATD7 = ~LATDbits.LATD7; } while(0)
+#define Display_1C_GetValue()           PORTDbits.RD7
+#define Display_1C_SetDigitalInput()    do { TRISDbits.TRISD7 = 1; } while(0)
+#define Display_1C_SetDigitalOutput()   do { TRISDbits.TRISD7 = 0; } while(0)
+#define Display_1C_SetAnalogMode()      do { ANSELDbits.ANSD7 = 1; } while(0)
+#define Display_1C_SetDigitalMode()     do { ANSELDbits.ANSD7 = 0; } while(0)
+
+/**
+   @Param
+    none
+   @Returns
+    none
+   @Description
+    GPIO and peripheral I/O initialization
+   @Example
+    PIN_MANAGER_Initialize();
+ */
+void PIN_MANAGER_Initialize (void);
+
+/**
+ * @Param
+    none
+ * @Returns
+    none
+ * @Description
+    Interrupt on Change Handling routine
+ * @Example
+    PIN_MANAGER_IOC();
+ */
+void PIN_MANAGER_IOC(void);
+
+
+
+#endif // PIN_MANAGER_H
+/**
+ End of File
+*/
