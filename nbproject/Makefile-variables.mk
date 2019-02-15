ca61cd922a9ee9e36df7d5168dfa258988da4ab1
Stephen Anthony <sxanthony06@gmail.com>
2019-02-14 22:09:35 -0400

    Revert "Merge branch 'development' of https://github.com/sxanthony06/DoubleDiceGame.X into development"
    This reverts commit a221dbdc40fa057896dbfd9795d5585975a2d10f, reversing
    changes made to 05279c3c6da934cebb1ff3530735ab49aff74c0c.

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,20 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# Simulator_PIC18F45K22 configuration
+CND_ARTIFACT_DIR_Simulator_PIC18F45K22=dist/Simulator_PIC18F45K22/production
+CND_ARTIFACT_NAME_Simulator_PIC18F45K22=DoubleDiceGame.X.production.hex
+CND_ARTIFACT_PATH_Simulator_PIC18F45K22=dist/Simulator_PIC18F45K22/production/DoubleDiceGame.X.production.hex
+CND_PACKAGE_DIR_Simulator_PIC18F45K22=${CND_DISTDIR}/Simulator_PIC18F45K22/package
+CND_PACKAGE_NAME_Simulator_PIC18F45K22=doubledicegame.x.tar
+CND_PACKAGE_PATH_Simulator_PIC18F45K22=${CND_DISTDIR}/Simulator_PIC18F45K22/package/doubledicegame.x.tar
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=DoubleDiceGame.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/DoubleDiceGame.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=doubledicegame.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/doubledicegame.x.tar
